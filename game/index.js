11dbb5a64c653ffa85a81bd3b61940cd0425246d
Martin Schreiber <martin@codelooksgood.com>
2014-09-13 00:27:39 +0200

    [r] renaming client side files

--- game/index.js
+++ game/index.js
@@ -3,7 +3,7 @@
 
 	var socket = window.io();
 	socket.on("newRoom", function(room) {
-		window.location.href += "host/index.html?id=" + room.id;
+		window.location.href += "host/host.html?id=" + room.id;
 	});
 
 	socket.on("currentRooms", function(rooms) {
@@ -12,7 +12,7 @@
 			return room1.id > room2.id;
 		}).forEach(function(room) {
 			var openSlots = 4 - Object.keys(room.players).length;
-			roomsHTML += "<a href='player/index.html?id=" + room.id + "'><button " + (openSlots === 0 ? "disabled" : "") + ">Game " + room.id + " (" + openSlots + " open slots) </button></a>";
+			roomsHTML += "<a href='player/player.html?id=" + room.id + "'><button " + (openSlots === 0 ? "disabled" : "") + ">Game " + room.id + " (" + openSlots + " open slots) </button></a>";
 		});
 
 		document.getElementById("rooms").innerHTML += roomsHTML;
